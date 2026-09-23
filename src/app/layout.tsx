0a4cfa76b721cbdd632d222e6690aff44d210bd8
Umar idris <111046815+Abbagigo13@users.noreply.github.com>
2026-09-23 13:09:55 +0100

    Change WalletProvider import to WalletContext

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { DashboardProvider } from "@/context/DashboardContext";
-import { WalletProvider } from "@/hooks/useWallet";
+import { WalletProvider } from "@/context/WalletContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
